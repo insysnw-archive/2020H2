8d665095be0bbcad6dbc3d556c23fec0dfa56db7
Mikhail Kazandzhi <666_mickhail@mail.ru>
2020-11-03 17:12:58 +0300

    Lab1-a solved

--- lab1/client.rb
+++ lab1/client.rb
@@ -9,29 +9,61 @@ class String
   end
 end
 
-print "Hello, client is starting\n"
-
-if ARGV.length < 2
-  warn "No server`s ip and port number in args\n"
-  exit 1
+def send(socket, msg)
+  socket.send(msg + "~@~", 0)
 end
 
-if (ARGV[0] =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/).nil?
-  warn "Server`s ip is not correct\n"
-  exit 1
-end
+begin
+  print "Hello, client is starting\n"
 
-unless ARGV[1].numeric?
-  warn "Port is not number\n"
-  exit 1
-end
+  if ARGV.length < 3
+    warn "No server`s ip, port number and nik in args\n"
+    exit 1
+  end
 
-socket = Socket.new(AF_INET, SOCK_STREAM, 0)
-sockaddr = Socket.pack_sockaddr_in(ARGV[1].to_i, ARGV[0])
-socket.connect(sockaddr)
-loop do
-  socket.puts '[123456879] 123456789'
-  print socket.recvfrom(256)[0].chomp
-  sleep 3
+  if (ARGV[0] =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/).nil?
+    warn "Server`s ip is not correct\n"
+    exit 1
+  end
+
+  unless ARGV[1].numeric?
+    warn "Port is not number\n"
+    exit 1
+  end
+
+  @work = true
+  nik = ARGV[2]
+
+  @socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+  sockaddr = Socket.pack_sockaddr_in(ARGV[1].to_i, ARGV[0])
+  @socket.connect(sockaddr)
+  output = Thread.new(nik) { |nik|
+    while @work do
+      text = STDIN.gets.chomp
+      send @socket, "[#{Time.now}] <#{nik}> #{text}"
+    end
+  }
+  input = Thread.new do
+    while @work do
+      text = ''
+      text << @socket.recvfrom(256)[0].chomp until text.end_with? "~@~"
+      print ">" + text[0..-4] + "\n"
+      exit if text == "Server is out. Please reconnect~@~"
+    end
+  end
+
+  def shutdown(input, output)
+    @work = false
+    input.exit
+    output.exit
+    @socket.close
+    exit 0
+  end
+
+  Signal.trap('INT') do
+    shutdown(input, output)
+  end
+
+  input.join
+  shutdown input, output
 end
-socket.close
--- lab1/server.rb
+++ lab1/server.rb
@@ -2,6 +2,7 @@ require 'socket'
 include Socket::Constants
 
 @clients = []
+threads = []
 
 class String
   def numeric?
@@ -14,27 +15,44 @@ end
 # @param [Socket] socket
 # @return [nil]
 def handler(socket)
+  socket.puts "Hello dear user~@~"
   loop do
     text = ''
-    text << socket.recvfrom(256)[0].chomp until text.include? "\n"
-    if text.index '[' != 0
+    until text.end_with? "~@~"
+      text << socket.recvfrom(256)[0].chomp
+    end
+    if text.index('[') != 0
       puts stderr 'Incorrect format of message'
     else
       dt = Time.at(text[1..text.index(']')].to_i)
-      text = format("[%s] %s\n", dt.to_s(':time'), text[text.index(']')..-2])
+      text = "[#{dt.strftime('%I:%M%p')}] #{text[text.index(']')+2..-1]}"
+      print text[0..-4] + "\n"
       @clients.each do |c|
         c.puts(text) if c != socket
       end
     end
+  rescue Errno::ECONNRESET
+    warn 'Client left as'
+    @clients.delete_at(@clients.index(socket))
+    socket.close
+  rescue Errno::EPIPE
+    @clients.delete_at(@clients.index(socket))
+    socket.close
+    exit
+  rescue IOError
+    exit
   end
-rescue Errno::ECONNRESET
-  warn 'Client left as'
-  @clients.delete_at(@clients.index(socket))
-  socket.close
 end
 
 def shut_down
-  @clients.each(&:close)
+  @clients.each do |c|
+    c.puts "Server is out. Please reconnect~@~"
+    c.close
+    rescue Errno::EPIPE
+      @clients.delete_at(@clients.index(c))
+      c.close
+      retry
+  end
   @socket.close
 end
 
@@ -52,7 +70,7 @@ end
 sockaddr = Socket.pack_sockaddr_in(ARGV[0].to_i, 'localhost')
 @socket.bind(sockaddr)
 @socket.listen(5)
-print "Server is online\n"
+print "Server is online. Press Ctrl+C to exit\n"
 Signal.trap('INT') do
   shut_down
   exit
