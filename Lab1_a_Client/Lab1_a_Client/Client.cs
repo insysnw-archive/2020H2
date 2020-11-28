517b44d78b7a39e13fef664853d807ca6801ea32
Алексей Пентегов <p.lexa.1999@gmail.com>
2020-11-28 15:24:31 +0300

    Lab 1 a

---
+++ Lab1_a_Client/Lab1_a_Client/Client.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient
+{
+    private static string name;
+    private static Socket sender;
+
+    public static bool StartClient()
+    {
+        try
+        {
+            // use port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.  
+            sender = new Socket(ipAddress.AddressFamily,
+               SocketType.Stream, ProtocolType.Tcp);
+            sender.Blocking = true;
+            try {
+                // Connect the socket to the remote endpoint.
+                sender.Connect(remoteEP);
+                //sender.Shutdown(SocketShutdown.Both);
+                //sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Connection Error");
+                Console.ReadLine();
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Connection Error");
+            Console.ReadLine();
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void SendMessage(string message)
+    {
+        byte[] bytes = new byte[1024];
+        byte[] msg = Encoding.ASCII.GetBytes($"< {DateTime.Now.ToString("hh:mm")} > [{name}] {message}<EOF>");
+  
+        int bytesSent = sender.Send(msg);
+    } 
+
+    private static void StartReceiving()
+    {
+        ThreadStart rec = new ThreadStart(Receiving);
+        Thread recThread = new Thread(rec);
+        recThread.Start();
+    }
+
+    private static void Receiving()
+    {
+        while (true)
+        {
+            try
+            {
+                Thread.Sleep(100);
+                byte[] bytes = new byte[1024];
+                int bytesRec = sender.Receive(bytes);
+                if (bytesRec > 0)
+                {
+                    var trimArr = new char[] { '<', 'E', 'O', 'F', '>' };
+                    var msg = Encoding.ASCII.GetString(bytes, 0, bytesRec).TrimEnd(trimArr);
+                    Console.WriteLine(msg);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Connection lost");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+
+    private static void InitClient()
+    {
+        Console.WriteLine("Hello. Enter your name");
+        name = Console.ReadLine();
+    }
+
+    public static int Main(String[] args)
+    {
+        InitClient();
+        var connected = StartClient();
+        if (!connected) return 0;
+
+        StartReceiving();
+
+        while (true)
+        {
+            var msg = Console.ReadLine();
+            SendMessage(msg);
+        }
+        return 0;
+    }
+}
---
+++ Lab1_a_Server/Lab1_a_Server/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketListener
+{
+    private static Socket listener;
+    private static List<Socket> clients;
+  
+    public static string data = null;
+
+    public static void StartServer()
+    {
+        clients = new List<Socket>();  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.  
+        listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine();
+        }
+    }
+
+    private static void AppendingConnections()
+    {
+
+        byte[] msg = Encoding.ASCII.GetBytes("One more joined this chat");
+        // Start listening for connections.  
+        while (true)
+        {
+            Console.WriteLine("Waiting for connection...");
+            // Program is suspended while waiting for an incoming connection.  
+            Socket handler = listener.Accept();
+            
+            clients.Add(handler);
+            Console.WriteLine("Count of clients: " + clients.Count);
+
+            foreach (Socket socket in clients)
+            {
+                socket.Send(msg);
+            }
+
+            StartReciving(handler);
+        }
+    }
+
+    private static void ReceiveMessage(Socket client)
+    {
+        byte[] bytes = new Byte[1024];
+        
+
+        // An incoming connection needs to be processed.  
+        while (true)
+        {
+            try
+            {
+
+                int bytesRec = client.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.IndexOf("<EOF>") > -1)
+                {
+
+                    foreach (Socket socket in clients)
+                    {
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                        if (socket != client)
+                            socket.Send(msg);
+                    }
+                    Console.WriteLine(data);
+                    data = null;
+                }
+            }
+            catch(SocketException e)
+            {
+                byte[] msg = Encoding.ASCII.GetBytes("Client leaved this chat");
+                clients.Remove(client);
+                foreach (Socket socket in clients)
+                {
+                    socket.Send(msg);
+                }
+                return;
+            }
+        }
+    }
+
+    private static void StartReciving(Socket client)
+    {
+        Thread conservationThread = new Thread(() => ReceiveMessage(client));
+        conservationThread.Start();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        StartServer();
+        AppendingConnections();
+        
+        return 0;
+    }
+}
