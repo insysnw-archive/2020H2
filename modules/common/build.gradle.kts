c72723c4c39193fcf2b3c1d51fc456becce72092
Кирилл Товпеко <ktlo@handtruth.com>
2020-12-13 14:42:18 +0000

    Common protocol described

--- modules/common/build.gradle.kts
+++ modules/common/build.gradle.kts
@@ -2,3 +2,9 @@ dependencies {
     api("io.ktor:ktor-network")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 }
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
+    this.dokkaSourceSets.configureEach {
+        includes.from("protocol.md")
+    }
+}
