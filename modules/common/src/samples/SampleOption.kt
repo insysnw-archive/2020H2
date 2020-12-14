e80c53725eb4343003ead8b4415acd8d53c9657b
ktlo <ktlo@handtruth.com>
2020-12-14 05:19:11 +0300

    Fix documentation sample fuckup

--- modules/common/src/samples/SampleOption.kt
+++ modules/common/src/samples/SampleOption.kt
@@ -6,7 +6,7 @@ import com.handtruth.net.lab3.types.*
 import io.ktor.utils.io.core.*
 
 fun sampleOption() {
-    // Пример использования классов Option и OptionFactory
+    // Пример использования классов Option и OptionCompanion
     data class SampleOption(val integer: Int, val string: String) : Option() {
         override fun write(output: Output) {
             output.writeVarInt(integer)
@@ -14,10 +14,10 @@ fun sampleOption() {
 
         // Эта аннотация добавляет имя класса этого companion объекта в
         // соответствующий файл в папке META-INF жарника. В рантайме все имена считываются
-        // и классы загружаются текущим класслоадером приложения. Объекты OptionFactory
+        // и классы загружаются текущим класслоадером приложения. Объекты OptionCompanion
         // в результате лежат в ассоциативном массиве allOptions.
-        @AutoService(OptionFactory::class)
-        companion object : OptionFactory(23) {
+        @AutoService(OptionCompanion::class)
+        companion object : OptionCompanion(23) {
             override fun read(input: ByteReadPacket) = with(input) {
                 SampleOption(readVarInt(), readVarString())
             }
