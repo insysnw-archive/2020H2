b29ae7870292f32431fef26d576ae1e0a20459f3
Алексей Пентегов <p.lexa.1999@gmail.com>
2021-01-18 13:48:32 +0300

    Lab2 DNS client and NTP server

---
+++ DnsClient/DnsClient/Program.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DnsClient
+{
+    class Program
+    {
+        private static Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static EndPoint serverPoint;
+        private static string serverAddress = "8.8.8.8";
+        private static DnsPacket packet;
+        private static bool error = false;
+
+        public static void Main(string[] arg)
+        {
+            
+            Console.WriteLine("Hi. It is Dns client" +
+                "\nWrite domain to get IP" +
+                "\nParameters after domain can change configuration" +
+                "\n-type <mx>. Type A by default" +
+                "\n-host <1.1.1.1>. Host 8.8.8.8 by default");
+
+            string line = "";
+            while(line != "exit")
+            {
+                packet = new DnsPacket();
+                packet.qtype = Type.A;
+                line = Console.ReadLine();
+
+                var param = line.Split(' ');
+
+                packet.qname = param[0];
+
+                for(int i = 0;i<param.Length;i++)
+                {
+                   
+
+                    if(param[i] == "-type")
+                    {
+                        if (param[i + 1].ToLower() == "mx")
+                            packet.qtype = Type.MX;
+                        if (param[i + 1].ToLower() == "txt")
+                            packet.qtype = Type.TXT;
+                    }
+                    if (param[i] == "-host")
+                    {
+                        if (IsValid(param[i + 1]))
+                        {
+                            serverAddress = param[i + 1];
+                        }
+                        else
+                        {
+                            Console.WriteLine("Host invalid");
+                            error = true;
+                            break;
+                        }
+                    }
+                }
+                if (error)
+                {
+                    error = false;
+                    continue;
+                }
+                packet.qr = QR.Request;
+                packet.opcode = Opcode.Query;
+                Client();
+            }
+            
+            
+            
+
+
+        }
+
+        public static bool IsValid(string str)
+        {
+            var b = str.Split('.');
+            if (b.Length != 4)
+                return false;
+
+            foreach(string s in b)
+            {
+                var i = 0;
+                if(!int.TryParse(s,out i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void Client()
+        {
+
+            
+            byte[] b = Pack(packet);
+
+            IPEndPoint ipRemoteEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), 53);
+            Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint ipLocalEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint localEndPoint = (EndPoint)ipLocalEndPoint;
+            udpClient.Bind(localEndPoint);
+
+            udpClient.Connect(ipRemoteEndPoint);
+            
+
+            //Console.WriteLine("Send to server");
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+            //Console.WriteLine("");
+            udpClient.Send(b);//, serverPoint);
+            byte[] r = new byte[512];
+            udpClient.Receive(r);//, ref serverPoint);
+            Unpack(r,b.Length);
+            //Console.WriteLine("Received from server");
+            for (int i = 0; i < r.Length; i++)
+            {
+                Console.Write(r[i] + " ");
+            }
+
+
+
+        }
+        
+
+        public static byte[] FakePack()
+        {
+            return new byte[] { 0xAA, 0xAA, 0x00, 0x0, 0x0, 0x01, 0x00, 0x0, 0x00, 0x0, 0x00, 0x00, 0x2, 0x76, 0x6b, 0x3, 0x63, 0x6f, 0x6d, 0x0, 0x0, 0x01, 0x00, 0x1 };
+        }
+
+
+        public static void Unpack(byte[] data,int requestLengh)
+        {
+            List<byte> res = new List<byte>();
+
+            for(int i = requestLengh+12;i < data.Length;i+=12)
+            {
+                if (data[i] == 0 && data[i + 1] == 0 && data[i + 2] == 0 && data[i + 3] == 0)
+                    break;
+                res.Add(data[i++]);
+                res.Add(data[i++]);
+                res.Add(data[i++]);
+                res.Add(data[i++]);
+                
+            }
+            int c = 0;
+            string ip = "";
+            Console.WriteLine("Host addr: " + serverAddress);
+            Console.WriteLine("Type: " + packet.qtype);
+            Console.WriteLine($"Results for request {packet.name}: " + res.Count/4);
+            foreach (byte b in res)
+            {
+                
+                if(c<3)
+                ip += b + ".";
+                else
+                {
+                    ip += b;
+                    Console.WriteLine(ip);
+                    ip = "";
+                    c = -1;
+                }
+                c++;
+            } 
+        }
+
+        public static byte[] Pack(DnsPacket packet)
+        {
+            var p = packet;
+
+
+            //byte[] bID = Encoding.UTF8.GetBytes("AAAA");//BitConverter.GetBytes(p.ID);
+            short parameters = (short)((byte)p.qr | (byte)p.opcode >> 1 | (byte)p.autorativeAnswer >> 5
+                               | (byte)p.trunCation >> 6 | (byte)p.recursionDesired >> 7
+                               | (byte)p.recursionAvailable >> 8 | (byte)p.reserved >> 9 | (byte)p.rcode >> 12);
+
+            //Console.WriteLine("Params " + parameters);
+
+            byte[] bParam = BitConverter.GetBytes(parameters);
+            byte[] bQd = BitConverter.GetBytes(p.qdCount);
+            byte[] bAn = BitConverter.GetBytes(p.anCount);
+            byte[] bSs = BitConverter.GetBytes(p.ssCount);
+            byte[] bAr = BitConverter.GetBytes(p.arCount);
+
+            string[] parts = p.qname.Split('.');
+            
+
+            byte[] bName = new byte[p.qname.Length+2];
+            var counter = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                bName[counter] = BitConverter.GetBytes(parts[i].Length)[0];
+                counter++;
+                var b = Encoding.Default.GetBytes(parts[i]);
+                for (int k = 0;k<b.Length;k++)
+                {
+                    bName[counter] = b[k];
+                    counter++;
+                }
+
+            }
+            bName[bName.Length - 1] = 0;
+            Console.WriteLine("Q TYpe " + (short)p.qtype);
+            byte[] bQType = BitConverter.GetBytes((short)p.qtype);
+            byte[] bQClass = BitConverter.GetBytes(p.qclass);
+
+            var nameSize = bName.Length;// % 16 + bName.Length;
+            byte[] dnsData = new byte[16 + nameSize];
+
+            dnsData[0] = 0xAA;
+            dnsData[1] = 0xAA;
+            dnsData[2] = bParam[0];
+            dnsData[3] = bParam[1];
+
+            dnsData[4] = bQd[1];
+            dnsData[5] = bQd[0];
+            dnsData[6] = bAn[0];
+            dnsData[7] = bAn[1];
+            dnsData[8] = bSs[0];
+            dnsData[9] = bSs[1];
+            dnsData[10] = bAr[0];
+            dnsData[11] = bAr[1];
+            
+            dnsData[12 + nameSize] = bQType[1];
+            dnsData[13 + nameSize] = bQType[0];
+            dnsData[14 + nameSize] = bQClass[1];
+            dnsData[15 + nameSize] = bQClass[0];
+            
+
+            for (int i = 0; i < bName.Length; i++)
+            {
+                dnsData[12 + i] = bName[i];
+            }
+
+            dnsData[11 + nameSize] = BitConverter.GetBytes((short)0)[0];
+
+            return dnsData;
+        }
+        
+    }
+    
+    public class DnsPacket
+    {
+        //header
+        public byte[] ID = new byte[2];                // 16 bit
+        public QR qr;                   // 1 bit
+        public Opcode opcode;           //4 bits
+        public AA autorativeAnswer;     // Равен 1 при ответе от сервера, в ведении которого находится домен, упомянутый в запросе.
+        public TC trunCation;           //Равен 1 при укорочении сообщения. Для UDP это означает, что ответ содержал более 512 октетов, но прислано только первые 512.
+        public RD recursionDesired;     // Равен 1, если для получения ответа желательна рекурсия.
+        public RA recursionAvailable;   //  Равен 1, если рекурсия для запрашиваемого сервера доступна.
+        public Z reserved = Z.False;    // Зарезервировано на будущее. Должны равняться нулю.
+        public Rcode rcode;
+
+        public short qdCount = 0x1; //should set this field to 1, indicating you have one question.
+        public short anCount = 0x0; //You should set this field to 0, indicating you are not providing any answers.
+        public short ssCount = 0x0; //You should set this field to 0, and should ignore any response entriesin this section.
+        public short arCount = 0x0; //You should set this field to 0,  and should ignore any response entries in thissection.
+
+        // question
+        public string qname;
+        public Type qtype;
+        public short qclass = 0x1;
+
+        //answer
+        public string name;
+        public short type;
+        public short aClass;
+        public int ttl;
+        public short rdLength;
+        public long rData;
+
+    }
+
+    public enum QR { Request = 0x0, Responce = 0x1 }
+    public enum Opcode { Query = 0x0, IQuery = 0x1, Status = 0x2 }
+    public enum AA { False = 0x0, True = 0x1 }
+    public enum TC { False = 0x0, True = 0x1 }
+    public enum RD { False = 0x0, True = 0x1 }
+    public enum RA { False = 0x0, True = 0x1 }
+    public enum Z { False = 0x0, True = 0x1 }
+    public enum Rcode { NoError = 0x0, RequestError = 0x1, ServerError = 0x2, NoSuchDomain = 0x3, CantProcessRequest = 0x4, Rejected = 0x5 }
+    public enum Type { A = 0x1, MX = 0xe, TXT = 0x10}
+
+
+       
+}
---
+++ NtpServer/NtpServer/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+//  E:\Education\7Sem\Nets\Labs2\NTP_Client.py
+
+namespace NtpServer
+{
+    class Program
+    {
+        private static Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Hi. It is NTP Server");
+            Thread conservationThread = new Thread(() => Server());
+            conservationThread.Start();
+            
+            Console.ReadKey();
+            
+        }
+
+        public static void Server()
+        {
+            _serverSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 123));
+
+            var ntpData = new byte[48];
+            NtpPacket n = new NtpPacket(3, Mode.Server);
+
+            while (true)
+            {
+                _serverSocket.ReceiveFrom(ntpData, ref epFrom);
+                long milliseconds = (long)(DateTime.Now - new DateTime(1900, 1, 1)).TotalMilliseconds;
+                n.receive = ConvertToNtp(milliseconds);
+                Pack(n, ntpData);
+                _serverSocket.SendTo(ntpData, epFrom);
+                
+            }
+        }
+
+        public static void Pack(NtpPacket ntp,byte[] ntpData)
+        {
+            ntpData[0] = (byte)((byte)ntp.mode | ntp.versionNumber << 3 | (byte) ntp.leapIndicator << 6); 
+
+            long milliseconds = (long)(DateTime.Now - new DateTime(1900, 1, 1)).TotalMilliseconds;
+            ntp.transmit = ConvertToNtp(milliseconds);
+
+            for (int i = 0; i <8; i++)
+            {
+                ntpData[i + 24] = ntpData[i+40]; //ordinate
+                ntpData[i+32] = ntp.receive[i]; // receive
+                ntpData[i+40] = ntp.transmit[i]; //transmit
+            }
+
+            
+        }
+
+        public static byte[] ConvertToNtp(long milliseconds)
+        {
+            var bytes = new byte[8];
+            
+            decimal intpart = 0, fractpart = 0;
+            
+            intpart = milliseconds / 1000;
+            fractpart = ((milliseconds % 1000) * 0x100000000L) / 1000m;
+            
+            var temp = intpart;
+            for (var i = 3; i >= 0; i--)
+            {
+                bytes[i] = (byte)(temp % 256);
+                temp = temp / 256;
+            }
+
+            temp = fractpart;
+            for (var i = 7; i >= 4; i--)
+            {
+                bytes[i] = (byte)(temp % 256);
+                temp = temp / 256;
+            }
+            return bytes;
+        }
+        
+    }
+
+
+
+    class NtpPacket
+    {
+        // Necessary of enter leap second (2 bits)
+        public LeapIndicator leapIndicator = LeapIndicator.NoCorrection;
+        // Version of protocol (3 bits)
+        public byte versionNumber = 3;
+        // Mode of sender (3 bits)
+        public Mode mode = Mode.Server;
+        // The level of "layering" reading time (1 byte)
+        public byte stratum = 0;
+        // Interval between requests (1 byte)
+        public byte pool = 0;
+        // Precision (log2) (1 byte)
+        public byte precision = 0;
+        // Interval for the clock reach NTP server (4 bytes)
+        public int rootDelay = 0;
+        // Scatter the clock NTP-server (4 bytes)
+        public int rootDispersion = 0;
+        // Indicator of clocks (4 bytes)
+        public int refId = 0;
+        // Last update time on server (8 bytes)
+        public byte[] reference = new byte[8];
+        // Time of sending packet from local machine (8 bytes)
+        public byte[] originate = new byte[8];
+        // Time of receipt on server (8 bytes)
+        public byte[] receive = new byte[8];
+        // Time of sending answer from server (8 bytes)
+        public byte[] transmit = new byte[8];
+
+
+        public NtpPacket(byte versionNum, Mode mode)
+        {
+            versionNumber = versionNum;
+            this.mode = mode;
+        }
+        
+    }
+
+    enum Mode {Reserved, SymActive,SymPassive , Client, Server, Broadcast , ControlMess,PrivateUsage}
+    enum LeapIndicator { NoCorrection,SecondsLastMin61, SecondsLastMin59,ServerError }
+}
